18be57d8f8a5f83a65acba356a9882cf50c201bc
조원근(Wonkeun, Cho) <wonkeun.cho@vatechewoo.com>
2024-05-24 18:01:07 +0900

    4. Add auth features ing

--- FrontEnd/Program.cs
+++ FrontEnd/Program.cs
@@ -1,6 +1,16 @@
 using FrontEnd.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using FrontEnd.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("IdentityDbContextConnection") ?? throw new InvalidOperationException("Connection string 'IdentityDbContextConnection' not found.");
+
+builder.Services.AddDbContext<IdentityDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<IdentityDbContext>();
 
 // Add services to the container.
 builder.Services.AddRazorPages();
@@ -8,6 +18,8 @@ builder.Services.AddHttpClient<IApiClient, ApiClient>(client =>
 {
     client.BaseAddress = new Uri(builder.Configuration["serviceUrl"]);
 });
+
+builder.Services.AddSingleton<IAdminService, AdminService>();
 
 var app = builder.Build();
 
@@ -23,6 +35,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
